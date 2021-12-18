57e22b06f271121ba1f26ecc446efb3af889cbe9
anthoz69 <st.anthoz69@gmail.com>
2021-12-18 09:16:53 +0700

    feat: landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
   purge: [
@@ -5,7 +7,23 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      'display': ['Karla', 'sans-serif'],  
+      'body': ['Poppins', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        amber: colors.amber
+      }
+    },
+    container: {
+      screens: {
+        "sm": "600px",
+        "md": "768px",
+        "lg": "1024px",
+        "xl": "1200px",
+      }
+    }
   },
   variants: {
     extend: {},
